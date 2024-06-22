0f664fcac46a298a701985e25b2e213bd20ce3a4
Tonon17 <tonondouglas3@gmail.com>
2024-06-22 11:50:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite outro número: ")) 
+
+if n1 > n2 :
+  print(f"{n1} é o maior número");
+
+if n1 < n2 :
+  print(f"{n2} é o maior número");
+
+if n1 == n2 :
+  print(f"Os números são iguais")
